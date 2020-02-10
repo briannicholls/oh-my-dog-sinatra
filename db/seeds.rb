4a02a484c5ab230d98a428e6f4ba35f60e56e289
Brian Nicholls <nichol88@gmail.com>
2020-02-10 02:42:59 -0500

    add robust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create(
 Dog.create(
   name: "Scooby Doo",
   breed: 'Great Dane',
-  birthday: '02-09-2020',
+  birthday: '2020-02-09',
   temperament: 'Friendly with people',
   notes: 'Scared of ghosts'
 )
@@ -21,6 +21,33 @@ Owner.create(
   zip_code: '08080',
   apartment_number: '1B',
   instructions: 'Under the stairs',
+  door_code: '1234',
+  lockbox_code: '4321'
+)
+
+User.create(
+  first_name: 'Ludwig',
+  last_name: 'Hernandez',
+  email: 'email@email.com'
+)
+
+Dog.create(
+  name: "Snoopy",
+  breed: 'Hound',
+  birthday: '02-09-2020',
+  temperament: 'Friendly with people',
+  notes: 'Likes birds'
+)
+
+Owner.create(
+  first_name: "Charlie",
+  last_name: 'Brown',
+  email: 'cb@email.com',
+  phone: '123-456-7890',
+  address: '123 Main St',
+  zip_code: '08080',
+  apartment_number: '1B',
+  instructions: 'Dog house in the back',
   door_code: '1234',
   lockbox_code: '4321'
 )
