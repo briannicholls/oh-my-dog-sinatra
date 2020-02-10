355e0a0d376723756d525ea9b237bd0ff90b8d1e
Brian Nicholls <nichol88@gmail.com>
2020-02-09 19:54:03 -0500

    add dogs controller

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,46 @@
+class DogsController < ApplicationController
+  get '/dogs' do
+    erb :'/dogs/index'
+  end
+
+  get '/dogs/new' do
+    erb :'/dogs/new'
+  end
+
+  post '/dogs' do
+    @dog = Dog.new(params[:dog])
+    if @dog.save
+      redirect '/dogs'
+    else
+      redirect '/dogs/new'
+    end
+  end
+
+  get '/dogs/:id' do
+    @dog = Dog.find_by(id: params[:id])
+    erb :'/dogs/show'
+  end
+
+  get '/dogs/:id/edit' do
+    @dog = Dog.find_by(id: params[:id])
+    erb :'/dogs/edit'
+  end
+
+  patch '/dogs/:id' do
+    @dog = Dog.find_by(id: params[:id])
+    if @dog and @dog.update(params[:dog])
+      redirect "/dogs/#{@dog.id}"
+    else
+      redirect "/dogs/#{@dog.id}/edit"
+    end
+  end
+
+  delete '/dogs/:id' do
+    @dog = Dog.find_by(id: params[:id])
+    if @dog.delete
+      redirect '/dogs'
+    else
+      redirect "/dogs/#{@dog.id}"
+    end
+  end
+end
