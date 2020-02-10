6c50c955c0ff58b0fa4070348470da482c1bb9e5
Brian Nicholls <nichol88@gmail.com>
2020-02-09 19:55:57 -0500

    configure initial application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+  end
 end
