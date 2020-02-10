4c7e39f2f7b138923bb4ff9d545d77ba1f96f652
Brian Nicholls <nichol88@gmail.com>
2020-02-09 21:22:38 -0500

    update birthday method in dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,4 +4,12 @@ class Dog < ActiveRecord::Base
   has_many :walks
   has_many :users, through: :walks
 
+  def birthdate
+    if self.birthday.respond_to?('strftime')
+      self.birthday.strftime('%Y-%m-%d')
+    else
+      self.birthday
+    end
+  end
+
 end
