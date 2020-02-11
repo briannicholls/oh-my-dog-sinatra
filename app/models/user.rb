6d38e2a2874e38b8afcffdfb660a6342e735c632
Brian Nicholls <nichol88@gmail.com>
2020-02-11 05:59:29 -0500

    add secure password and params validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-#  has_secure_password
-#  validates :email, presence: :true, uniqueness: :true
-#  validates :password, presence: :true, confirmation: :true
-#  validates_confirmation_of :password
+   has_secure_password
+   validates :email, presence: :true, uniqueness: :true
+   validates :password, presence: :true, confirmation: :true
+   validates_confirmation_of :password
 
   has_many :walks
   has_many :dogs, through: :walks # Walk = "user_dog"
