beabaeca935963f176fba413d9b5b3a77b546753
Brian Nicholls <nichol88@gmail.com>
2020-02-11 05:53:34 -0500

    set :database correctly

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,19 @@ ENV['SINATRA_ENV'] ||= 'development'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+# for ActiveRecord 6+
+
+  set :database_file, './database.yml'
+
+# set :database, {
+#   adapter: "sqlite3",
+#   database: "db/#{ENV['SINATRA_ENV']}.sqlite3"
+# }
 
 require_all 'app'
 require_all 'lib'
