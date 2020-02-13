4f1b4a7b8636b2f12ab13d7ea6dea0d683c39b88
Brian Nicholls <nichol88@gmail.com>
2020-02-13 11:54:40 -0500

    fix rake db:seed (neighborhood error)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 ContactsImporter.import_owners_from_csv
 ContactsImporter.import_dogs_from_csv
+Owner.all.each do |owner|
+  if owner.neighborhood.to_s.length > 50
+    owner.neighborhood = nil
+    owner.save
+  end
+end
+User.create(
+  first_name: "admin",
+  email: 'admin@email.com',
+  password: 'password'
+)
