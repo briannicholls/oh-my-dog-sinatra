20449c560cbdf0f43a021a88b7b927cc20d1a20b
Brian Nicholls <nichol88@gmail.com>
2020-02-18 13:57:06 -0500

    only include dotenv in development

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 ENV['SINATRA_ENV'] ||= 'development'
-#require 'dotenv/load' if ENV['SINATRA_ENV'] == 'development'
+require 'dotenv/load' if ENV['SINATRA_ENV'] == 'development'
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
