0ceb8d3b73290fa08239fb9012cbeb1c8dc6cb73
Brian Nicholls <nichol88@gmail.com>
2020-02-17 20:02:13 -0500

    add details to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_024940) do
+ActiveRecord::Schema.define(version: 2020_02_17_042348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_02_16_024940) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
+    t.string "phone"
+    t.string "status"
   end
 
   create_table "walks", force: :cascade do |t|
